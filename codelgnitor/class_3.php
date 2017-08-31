fb0a17b69026ea39a291fe715631b2976f82d940
Rayhan09 <rayhansh1@gmail.com>
2017-08-31 12:17:13 +0600

    31/8 frist

--- codelgnitor/class_3.php
+++ codelgnitor/class_3.php
@@ -5,8 +5,13 @@
     <head>
         <title> Learn Codelginitor</title>
         <meta charset="UTF-8">
-        <meta>
-
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <link rel="icon" href="" type="image/x-icon">
     </head>
+
+    <body>
+
+
+    </body>
 
 </html>
