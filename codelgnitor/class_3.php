9f2919f034c226d597e7f6b3faa32b002943f65a
Rayhan09 <rayhansh1@gmail.com>
2017-09-01 19:55:32 +0600

    1/9 frist

--- codelgnitor/class_3.php
+++ codelgnitor/class_3.php
@@ -10,8 +10,28 @@
     </head>
 
     <body>
+        <table border="1" align="center" width="980px" height="1">
+            <tr>
+                <td>Logo</td>
+                <td>Banner</td>
 
+            </tr>
 
+            <tr>
+                <td>Menu</td>
+            </tr>
+
+            <tr>
+                <td>Left</td>
+                <td>Content</td>
+                <td>Right</td>
+            </tr>
+
+            <tr>
+                <td>Footer</td>
+            </tr>
+
+        </table>
     </body>
 
 </html>
