61fd2eaede3d5f969e75fccf2aeee7419c643793
Rayhan09 <rayhansh1@gmail.com>
2017-09-01 20:26:34 +0600

    1/9 2nd

--- codelgnitor/class_3.php
+++ codelgnitor/class_3.php
@@ -10,25 +10,128 @@
     </head>
 
     <body>
-        <table border="1" align="center" width="980px" height="1">
+        <table border="1" align="center" width="980px" height="980">
             <tr>
-                <td>Logo</td>
-                <td>Banner</td>
+                <td height="150px" width="150px">Logo</td>
+                <td colspan="2">Banner</td>
 
             </tr>
 
             <tr>
-                <td>Menu</td>
+                <td colspan="3" height="50px">Menu</td>
             </tr>
 
             <tr>
                 <td>Left</td>
-                <td>Content</td>
-                <td>Right</td>
+                <td valign="top">
+                    <h3> IT Training</h3>
+                        <p>Training is teaching, or developing in oneself or others, any skills and knowledge that relate to specific useful competencies. Training has specific goals of improving one's capability, capacity, productivity and performance.</p>
+
+                    <h3>PHP</h3>
+                        <p>Training Overview
+
+                            As wonderful as Laravel is, it does assume that you have a basic understanding of object-oriented. If you enjoy this style of learning, TalhaTraining is the missing piece in learning Laravel. We provide professional specialized training in Laravel.
+
+                            Our mission is to help people learning Laravel and building awesome websites. In this course, we provide all information about Laravel. Learn everything at one place.
+
+
+
+                            Training Objectives
+
+                            Complete web development package with learn Laravel from scratch.
+                            Building a Inventory System with Calendar, Customers Management, Account Management, Inventory Management, Invoices Management and Expenses Management.
+                            Object Oriented PHP
+                            JavaScript, Ajax & jQuery Design
+                            SSL and email server.
+                            Online payment methods PayPal Standard and Authorize.net.
+                            Making practical apps to bring your skills to the next level you will complete Individual / Group Project using PHP framework Laravel with MySQL.
+
+
+                            Prerequisites
+
+                            All Laravel - the PHP Framework for Web Artisans attendees should have practical experience of writing web applications in PHP.
+
+
+
+                            Hands-on/Lecture Ratio
+
+                            This training class is 80% hands-on, 20% lecture. Students learn by doing, with immediate opportunities to apply the material they learn to real-world problems.
+
+
+
+                            Training Materials
+
+                            All related software and lecture sheets and free e-book will provide in class.
+
+
+
+                            Training Outline
+
+                            Laravel Framework – Basic PHP with Framework
+
+                            About PHP & MySQL
+                            Advantage of using Framework
+                            Introducing Laravel Framework</p>
+                    <h3>Python</h3>
+                        <p>Prerequisites
+
+                            You should have a basic understanding of programming language and any text editor.
+
+
+
+                            Hands-on/Lecture Ratio
+
+                            This training class is 80% hands-on, 20% lecture. Students learn by doing, with immediate opportunities to apply the material they learn to real-world problems.
+
+
+
+                            Training Materials
+
+                            All related software and lecture sheets and free e-book will provide in class.
+
+
+
+                            Training Outline
+
+                            Python Basic
+
+                            About Python
+                            Python - Environment Setup
+                            Python Basic Syntax
+                            Python Variable and Operator Overview
+
+                            About Variable
+                            Variable Types
+                            Operator
+                            Arithmetic Operators
+                            Comparison (Relational) Operators
+                            Assignment Operators
+                            Logical Operators
+                            Bitwise Operators
+                            Membership Operators
+                            Identity Operators
+                            Python Statement Overview
+
+                            Statement Overview
+                            If Statements
+                            If...Else Statements
+                            Nested If Statements</p>
+                    <h3></h3>
+                    <h3></h3>
+                    <h3></h3>
+                    <h3></h3>
+                    <h3></h3>
+                    <h3></h3>
+                    <h3></h3>
+                    <h3></h3>
+
+
+                </td>
+                <td width="150px">Right</td>
             </tr>
 
             <tr>
-                <td>Footer</td>
+                <td colspan="3" height="150px">Footer</td>
             </tr>
 
         </table>
