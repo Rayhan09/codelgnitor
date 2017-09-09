abb21715503077393bda02cfe7bb5eac1427d070
Rayhan09 <rayhansh1@gmail.com>
2017-09-09 21:43:39 +0600

    9/9 1st

--- codelgnitor/class_3.php
+++ codelgnitor/class_3.php
@@ -116,7 +116,37 @@
                             If Statements
                             If...Else Statements
                             Nested If Statements</p>
-                    <h3></h3>
+                    <h3>Java J2EE</h3>
+                        <p>
+                            Java Platform, Enterprise Edition (Java EE)
+                            The Java Platform, Enterprise Edition (Java EE) is a collection of Java APIs owned by Oracle that software developers can use to write server-side applications. It was formerly known as Java 2 Platform, Enterprise Edition, or J2EE.
+                            DEFINITION
+                            Java Platform, Enterprise Edition (Java EE)
+
+                            Posted by: Margaret Rouse
+                            WhatIs.com
+
+                            Contributor(s): Cameron McKenzie
+
+                            The Java Platform, Enterprise Edition (Java EE) is a collection of Java APIs owned by Oracle that software developers can use to write server-side applications. It was formerly known as Java 2 Platform, Enterprise Edition, or J2EE.
+
+
+                            Download this free guide
+                            Free Handbook: Java App Development in the Cloud
+                            Software engineers are approaching development and enterprise design in an entirely new way, thanks to the cloud. In this expert handbook, explore how your peers are leveraging the cloud to streamline app lifecycle management, save money, and make production and security more efficient.
+                            Start Download
+
+                            Sun Microsystems (together with industry partners such as IBM) originally designed Java EE to simplify application development in a thin-client-tiered environment. Java EE simplifies app development and decreases the need for programming by creating standardized, reusable modular components and by enabling the tier to handle many aspects of programming automatically.
+
+                            Java EE applications are hosted on application servers, such as  IBM's WebSphere, Oracle's GlassFish  or Red Hat's WildFly server, all of which run either in the cloud or within a corporate data center. While Java EE apps are hosted on the server side, examples of Java EE clients include an internet of things (IoT) device, smartphone, RESTful web service, standard web-based application, WebSocket or even microservices running in a Docker container.
+
+                            Java EE architecture goals
+                            The Java EE architecture provides services that simplify the most common challenges facing developers when building modern applications, in many cases through APIs, thus making it easier to use popular design patterns and industry-accepted best practices.
+
+                            For example, one common challenge enterprise developers face is how to handle requests coming in from web-based clients. To simplify this challenge, Java EE provides the Servlet and JavaServer Pages (JSP) APIs, which provide methods for activities like finding out what a user typed into a text field in an online form or storing a cookie on a user's browser.
+
+                            Another common task is how to store and retrieve information in a database. To address this goal, Java EE provides the Java Persistence API (JPA,) which makes it easy to map data used within a program to information stored in the tables and rows of a database. Also, creating web services or highly scalable logic components is simplified through the use of the Enterprise JavaBeans (EJB) specification. All of these APIs are well tested, relatively easy for Java developers to learn and can greatly simplify some of the hardest parts of enterprise development.
+                        </p>
                     <h3></h3>
                     <h3></h3>
                     <h3></h3>
