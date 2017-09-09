2a5199aae6f4d712579e33860718be22ae0a830f
Rayhan09 <rayhansh1@gmail.com>
2017-09-10 02:26:37 +0600

    10/9 2nd

--- codelgnitor/class_3.php
+++ codelgnitor/class_3.php
@@ -10,9 +10,9 @@
     </head>
 
     <body>
-        <table border="1" align="center" width="980px" height="980">
+        <table cellpadding="10px" border="1" align="center" width="980px" height="980">
             <tr>
-                <td height="150px" width="150px">Logo</td>
+                <td height="100px" width="100px">Logo</td>
                 <td colspan="2">Banner</td>
 
             </tr>
@@ -25,7 +25,7 @@
                 <td>Left</td>
                 <td valign="top">
                     <h3> IT Training</h3>
-                        <p>Training is teaching, or developing in oneself or others, any skills and knowledge that relate to specific useful competencies. Training has specific goals of improving one's capability, capacity, productivity and performance.</p>
+                        <p align="justify">Training is teaching, or developing in oneself or others, any skills and knowledge that relate to specific useful competencies. Training has specific goals of improving one's capability, capacity, productivity and performance.</p>
 
                     <h3>PHP</h3>
                         <p align="justify">Training Overview
@@ -45,26 +45,12 @@
                             SSL and email server.
                             Online payment methods PayPal Standard and Authorize.net.
                             Making practical apps to bring your skills to the next level you will complete Individual / Group Project using PHP framework Laravel with MySQL.
-
-
                             Prerequisites
-
                             All Laravel - the PHP Framework for Web Artisans attendees should have practical experience of writing web applications in PHP.
-
-
-
                             Hands-on/Lecture Ratio
-
                             This training class is 80% hands-on, 20% lecture. Students learn by doing, with immediate opportunities to apply the material they learn to real-world problems.
-
-
-
                             Training Materials
-
                             All related software and lecture sheets and free e-book will provide in class.
-
-
-
                             Training Outline
 
                             Laravel Framework – Basic PHP with Framework
@@ -76,8 +62,6 @@
                         <p align="justify">Prerequisites
 
                             You should have a basic understanding of programming language and any text editor.
-
-
 
                             Hands-on/Lecture Ratio
 
@@ -161,7 +145,7 @@
             </tr>
 
             <tr>
-                <td colspan="3" height="150px">Footer</td>
+                <td colspan="3" height="80px">Footer</td>
             </tr>
 
         </table>
