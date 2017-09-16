ddd8e5f6ec3abbac1b909e93bedc47c049516c07
Rayhan09 <rayhansh1@gmail.com>
2017-09-16 12:38:34 +0600

    16/9 1st

--- codelgnitor/class-4/class_4.php
+++ codelgnitor/class-4/class_4.php
@@ -6,7 +6,7 @@
         <title> Learn Codelginitor</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
-        <link rel="icon" href="" type="image/x-icon">
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
     </head>
 
     <body>
