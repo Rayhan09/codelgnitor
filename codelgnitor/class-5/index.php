332ac36179038ac55bd54ec603edde7bebc4a28b
Rayhan09 <rayhansh1@gmail.com>
2017-11-14 19:16:38 +0600

    14/11 2nd

---
+++ codelgnitor/class-5/index.php
@@ -0,0 +1,224 @@
+<?php
+
+?>
+<html>
+    <head>
+        <title> Learn Codelginitor</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+        <style>
+            body{
+                background-color: lightgoldenrodyellow;
+            }
+            table{
+                background-color:palegreen;
+            }
+            p{
+                color:gray;
+            }
+            .test{
+                color: beige;
+                font-family: "Comic Sans MS";
+                font-size: 100%;
+            }
+        </style>
+    </head>
+
+    <body>
+        <table cellpadding="10px" border="1" align="center" width="980px" height="980">
+            <tr>
+                <td id="logo" height="100px" width="100px">
+                    <img src="images 123.jpg" width="100" height="100">
+                </td>
+                <td colspan="2">
+                    <img src="Banner-5-dotnet-nuke-nopcommerce.jpg" width="980" height="100" >
+                </td>
+
+            </tr>
+
+            <tr>
+                <td colspan="3" height="50px">
+                    <a href="index.php">Home</a>
+                    <a href="sign_up.html">Sign up</a>
+                    <a href="about_us.php">About us</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td width="80px">Left</td>
+                <td valign="top">
+                    <h3> IT Training</h3>
+                        <p align="justify">Training is teaching, or developing in oneself or others, any skills and knowledge that relate to specific useful competencies. Training has specific goals of improving one's capability, capacity, productivity and performance.</p>
+
+                    <h3>PHP</h3>
+                        <p align="justify" class="test">Training Overview
+
+                            As wonderful as <b> <a href="https://laravel.com/" target="_blank">Laravel</a> </b> is, it does assume that you have a basic understanding of object-oriented. If you enjoy this style of learning, TalhaTraining is the missing piece in learning Laravel. We provide professional specialized training in Laravel.
+
+                            Our mission is to help people learning Laravel and building awesome websites. In this course, we provide all information about Laravel. Learn everything at one place.
+
+
+
+                            <strong>Training Objectives</strong>
+
+                            <i>Complete web development</i> package with learn Laravel from scratch.
+                            Building a Inventory System with Calendar, Customers Management, Account Management, Inventory Management, Invoices Management and Expenses Management.
+                            <em>Object Oriented PHP
+                            JavaScript, Ajax & jQuery Design
+                                SSL and email server.</em>
+                            <u>Online payment methods PayPal Standard and Authorize.net.</u>
+                            Making practical apps to bring your skills to the next level you will complete Individual / Group Project using PHP framework Laravel with MySQL.
+                            Prerequisites
+                            <mark>All Laravel - the PHP Framework for Web Artisans attendees should have practical experience of writing web applications in PHP.
+                                Hands-on/Lecture Ratio</mark>
+                            This training class is 80% hands-on, 20% lecture. Students learn by doing, with immediate opportunities to apply the material they learn to real-world problems.
+                            Training Materials
+                            All related software and lecture sheets and free e-book will provide in class.
+                            Training Outline
+
+                            <big>Laravel Framework</big> – Basic <abbr title=" PHP Hypertext Preprocessor programming language">PHP</abbr> with Framework<br>
+
+                            About <small>PHP & MySQL</small>
+                            Advantage of using Framework
+                            Introducing Laravel Framework<br/>
+                    (a+b)<sup>2</sup><br/><hr/>Log10<sub>2</sub>
+                        </p>
+                    <h3>Python</h3>
+                        <p align="justify">Prerequisites
+
+                            You should have a basic understanding of programming language and any text editor.
+
+                            Hands-on/Lecture Ratio
+
+                            This training class is 80% hands-on, 20% lecture. Students learn by doing, with immediate opportunities to apply the material they learn to real-world problems.
+
+
+
+                            Training Materials
+
+                            All related software and lecture sheets and free e-book will provide in class.
+
+
+
+                            Training Outline
+
+                            Python Basic
+
+                            About Python
+                            Python - Environment Setup
+                            Python Basic Syntax
+                            Python Variable and Operator Overview
+
+                            About Variable
+                            Variable Types
+                            Operator
+                            Arithmetic Operators
+                            Comparison (Relational) Operators
+                            Assignment Operators
+                            Logical Operators
+                            Bitwise Operators
+                            Membership Operators
+                            Identity Operators
+                            Python Statement Overview
+
+                            Statement Overview
+                            If Statements
+                            If...Else Statements
+                            Nested If Statements</p>
+                    <h3>Java J2EE</h3>
+                        <p align="justify">
+                            Java Platform, Enterprise Edition (Java EE)
+                            The Java Platform, Enterprise Edition (Java EE) is a collection of Java APIs owned by Oracle that software developers can use to write server-side applications. It was formerly known as Java 2 Platform, Enterprise Edition, or J2EE.
+                            DEFINITION
+                            Java Platform, Enterprise Edition (Java EE)
+
+                            Posted by: Margaret Rouse
+                            WhatIs.com
+
+                            Contributor(s): Cameron McKenzie
+
+                            The Java Platform, Enterprise Edition (Java EE) is a collection of Java APIs owned by Oracle that software developers can use to write server-side applications. It was formerly known as Java 2 Platform, Enterprise Edition, or J2EE.
+
+
+                            Download this free guide
+                            Free Handbook: Java App Development in the Cloud
+                            Software engineers are approaching development and enterprise design in an entirely new way, thanks to the cloud. In this expert handbook, explore how your peers are leveraging the cloud to streamline app lifecycle management, save money, and make production and security more efficient.
+                            Start Download
+
+                            Sun Microsystems (together with industry partners such as IBM) originally designed Java EE to simplify application development in a thin-client-tiered environment. Java EE simplifies app development and decreases the need for programming by creating standardized, reusable modular components and by enabling the tier to handle many aspects of programming automatically.
+
+                            Java EE applications are hosted on application servers, such as  IBM's WebSphere, Oracle's GlassFish  or Red Hat's WildFly server, all of which run either in the cloud or within a corporate data center. While Java EE apps are hosted on the server side, examples of Java EE clients include an internet of things (IoT) device, smartphone, RESTful web service, standard web-based application, WebSocket or even microservices running in a Docker container.
+
+                            Java EE architecture goals
+                            The Java EE architecture provides services that simplify the most common challenges facing developers when building modern applications, in many cases through APIs, thus making it easier to use popular design patterns and industry-accepted best practices.
+
+                            For example, one common challenge enterprise developers face is how to handle requests coming in from web-based clients. To simplify this challenge, Java EE provides the Servlet and JavaServer Pages (JSP) APIs, which provide methods for activities like finding out what a user typed into a text field in an online form or storing a cookie on a user's browser.
+
+                            Another common task is how to store and retrieve information in a database. To address this goal, Java EE provides the Java Persistence API (JPA,) which makes it easy to map data used within a program to information stored in the tables and rows of a database. Also, creating web services or highly scalable logic components is simplified through the use of the Enterprise JavaBeans (EJB) specification. All of these APIs are well tested, relatively easy for Java developers to learn and can greatly simplify some of the hardest parts of enterprise development.
+                        </p>
+                    <h3>Which language you like</h3>
+                    <ol>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JS</li>
+                        <li>PHP</li>
+                        <li>JAVA - </li>
+                        <ol>
+                            <li>Core java</li>
+                            <li>J2EE</li>
+                        </ol>
+                        <li>Python</li>
+                    </ol>
+
+                    <h3>Which language you like</h3>
+                    <ol>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JS</li>
+                        <li>PHP</li>
+                        <li>JAVA -
+                            <ol type="A">
+                                <li>Core Java</li>
+                                <li>Java Level
+                                    <ol type="a">
+                                        <li>Beganer</li>
+                                        <li>Medium</li>
+                                        <li>Expert</li>
+                                    </ol>
+                                </li>
+                            </ol>
+                        </li>
+
+                        <li>Python</li>
+                    </ol>
+                    <h3>Which language you like -</h3>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JS</li>
+                        <li>Java</li>
+                        <li>Python</li>
+
+                    </ul>
+                    <h3></h3>
+                    <h3></h3>
+                    <h3></h3>
+                    <h3></h3>
+
+
+                </td>
+                <td width="110px">Right</td>
+            </tr>
+
+            <tr>
+                <td colspan="3" height="80px" >
+                    <a  href="#logo">&and;</a>
+                </td>
+            </tr>
+
+        </table>
+    </body>
+
+</html>
