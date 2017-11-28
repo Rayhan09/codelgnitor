af5d0bd53b1e5a4be558e26f11310f16337dab4a
Rayhan09 <rayhansh1@gmail.com>
2017-11-28 13:00:25 +0600

    28/11 1st

---
+++ codelgnitor/class-7/layout/index.php
@@ -0,0 +1,33 @@
+<?php?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>codelgnitor</title>
+        <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
+        <link type="text/css" href="css/style.css" rel="stylesheet">
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+    </head>
+
+    <body>
+        <div id="wrapper">
+            <div id="header_wrapper">
+                <div id="logo"> </div>
+                <div id="banner"></div>
+            </div>
+            <div id="menu">
+
+            </div>
+            <div id="content_wrapper">
+                <div id="left"></div>
+                <div id="content">
+                <p> this is content</p>
+                </div>
+<!--                <div id="right"></div>-->
+            </div>
+
+            <div id="footer"></div>
+
+        </div>
+
+    </body>
+</html>
